a5d952bea07a6228bc3c3fdea1ace9d0d70f2230
Rik Langeveld <Rik_langeveld@hotmail.com>
2025-01-31 20:15:51 +0100

    tg group?

---
+++ Assets/Scripts/TargetGroupManager.cs
@@ -0,0 +1,17 @@
+using Cinemachine;
+using UnityEngine;
+
+public class TargetGroupManager : MonoBehaviour
+{
+   [SerializeField]
+   CinemachineTargetGroup targetGroup;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+      GameObject[] players =  GameObject.FindGameObjectsWithTag("Player");
+      foreach (var player in players)
+      {
+         targetGroup.AddMember(player.transform, 1, 1);
+      }
+    }
+}
