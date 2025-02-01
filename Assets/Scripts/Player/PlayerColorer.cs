e42f1cb3ac97f95e88c5a8d45639f0d218f30ef3
MarGama <210358@student.glu.nl>
2025-02-01 07:12:09 +0100

    colored bubbles :)

--- Assets/Scripts/Player/PlayerColorer.cs
+++ Assets/Scripts/Player/PlayerColorer.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 
 public class PlayerColorer : MonoBehaviour
 {
-   [SerializeField] private SkinnedMeshRenderer[] rends;
+   [SerializeField] private SkinnedMeshRenderer[] wizardRends;
+   [SerializeField] private MeshRenderer bubbleRend;
 
    public void ColorWizard(Material color)
    {
-      foreach (var rend in rends)
+      foreach (var rend in wizardRends)
       {
          rend.material = color;
       }
+   }
+
+   public void ColorBubble(Material color)
+   {
+      bubbleRend.material = color;
    }
 }
