b9977193d68773bd18058516ee2f37a204457ac6
MarGama <210358@student.glu.nl>
2025-02-01 06:05:01 +0100

    yup

---
+++ Assets/Scripts/SFX/ButtonSelectedAudioTrigger.cs
@@ -0,0 +1,19 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ButtonSelectedAudioTrigger : MonoBehaviour
+{
+   Button button;
+
+   private void Start()
+   {
+      button = GetComponent<Button>();
+   }
+
+   private void Update()
+   {
+      
+   }
+}
