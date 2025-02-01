0685d8abe5dd4b5b3d5626b4e048a032d14db5c9
Noah Beij <noahbeij@gmail.com>
2025-02-01 10:57:12 +0100

    Menu UI

--- Assets/Scripts/SFX/ButtonSelectedAudioTrigger.cs
+++ Assets/Scripts/SFX/ButtonSelectedAudioTrigger.cs
@@ -4,24 +4,53 @@ using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
 public class ButtonSelectedAudioTrigger : MonoBehaviour
 {
    EventSystem eventSystem;
    private GameObject selectedObject;
    [SerializeField] private AudioClip selectAudioClip;
+   [SerializeField] private TMP_Text text;
 
    private void Start()
    {
       eventSystem = FindAnyObjectByType<EventSystem>();
       selectedObject = eventSystem.currentSelectedGameObject;
+
+
    }
+
+   private GameObject previousSelectedObject;
 
    private void Update()
    {
       if (eventSystem.currentSelectedGameObject != selectedObject)
       {
          PlaySfx.instance.playOneShotSFX(selectAudioClip, transform, 2, 2, 1, 1, 0);
+
+         // Reset the font style of the previously selected object
+         if (previousSelectedObject != null)
+         {
+            var previousText = previousSelectedObject.GetComponentInChildren<TextMeshProUGUI>();
+            if (previousText != null)
+            {
+               previousText.fontStyle = FontStyles.Normal;
+            }
+         }
+
+         // Underline the currently selected object
          selectedObject = eventSystem.currentSelectedGameObject;
+         var currentText = selectedObject.GetComponentInChildren<TextMeshProUGUI>();
+         if (currentText != null)
+         {
+            currentText.fontStyle = FontStyles.Underline;
+         }
+
+         // Update the previous selected object
+         previousSelectedObject = selectedObject;
       }
    }
 }
