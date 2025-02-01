ac061b2a3d31d9aca446e30f5f0f1069a95ae607
Noah Beij <noahbeij@gmail.com>
2025-02-01 01:23:57 +0100

    player burst sfx

--- Assets/Scripts/Player/PlayerSounds.cs
+++ Assets/Scripts/Player/PlayerSounds.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class PlayerSounds : MonoBehaviour
 {
-    [Tooltip("The audio source for when the player bubble is colliding with another player bubble")]
+    [Tooltip("The audio for when the player bubble is colliding with another player bubble")]
     [SerializeField] private AudioClip[] B2bCollisions;
+
+    [Tooltip("The audio for when a players bubble bursts")]
+    [SerializeField] private AudioClip[] Burst;
 
 
     void OnCollisionEnter(Collision collision)
@@ -12,6 +15,11 @@ public class PlayerSounds : MonoBehaviour
         {
             PlaySfx.instance.playOneShotSFX(B2bCollisions[Random.Range(0, B2bCollisions.Length)], transform, 0.8f, 1.0f, 0.8f, 1.2f, 1f);
         }
+    }
+
+    public void onDeath()
+    {
+        PlaySfx.instance.playOneShotSFX(Burst[Random.Range(0, Burst.Length)], transform, 0.8f, 1.0f, 0.8f, 1.2f, 1f);
     }
 
 }
